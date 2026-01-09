2613cca5376bb7cb9d20cd8f4000c217d8cb0df0
Sharesth <sharesth444yt@gmail.com>
2026-01-09 23:42:40 +0530

    added a new part of code

---
+++ tests/test_sabr.py
@@ -0,0 +1,8 @@
+from models.sabr import sabr_implied_vol
+
+def test_sabr_positive_vol():
+    vol = sabr_implied_vol(
+        F=100, K=100, T=1,
+        alpha=0.3, beta=0.5, rho=-0.2, nu=0.4
+    )
+    assert vol > 0
