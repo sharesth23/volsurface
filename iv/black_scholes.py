bcbb7eda889dd3e70f8ac6c7ad08062a84de4bd0
Sharesth <sharesth444yt@gmail.com>
2026-01-09 18:12:40 +0530

    added code in the file

---
+++ iv/black_scholes.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.stats import norm 
+
+def bs_call_price(S, K, T, r, sigma):
+    if T <= 0 or sigma <= 0:
+        return max(S - K, 0)
+
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
