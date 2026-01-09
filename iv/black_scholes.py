a3f59d4629e311d57c1604f69c33d268b46919c6
Sharesth <sharesth444yt@gmail.com>
2026-01-09 18:13:19 +0530

    added a bew function

--- iv/black_scholes.py
+++ iv/black_scholes.py
@@ -9,3 +9,20 @@ def bs_call_price(S, K, T, r, sigma):
     d2 = d1 - sigma * np.sqrt(T)
 
     return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
+def implied_volatility(price, S, K, T, r, tol=1e-6):
+    low, high = 1e-6, 5.0
+
+    for _ in range(100):
+        mid = 0.5 * (low + high)
+        val = bs_call_price(S, K, T, r, mid)
+
+        if abs(val - price) < tol:
+            return mid
+
+        if val > price:
+            high = mid
+        else:
+            low = mid
+
+    return mid
