1850015e0c6c614f4a6a8e6b0220f51dad197533
Sharesth <sharesth444yt@gmail.com>
2026-01-09 18:25:06 +0530

    added a new function

--- models/sabr.py
+++ models/sabr.py
@@ -1,2 +1,11 @@
 import numpy as np
 from scipy.optimize import minimize
+
+def sabr_vol(F, K, T, alpha, beta, rho, nu):
+    if F == K:
+        return alpha / (F ** (1 - beta))
+
+    z = (nu / alpha) * (F * K) ** ((1 - beta) / 2) * np.log(F / K)
+    x = np.log((np.sqrt(1 - 2 * rho * z + z**2) + z - rho) / (1 - rho))
+
+    return (alpha / ((F * K) ** ((1 - beta) / 2))) * (z / x)
