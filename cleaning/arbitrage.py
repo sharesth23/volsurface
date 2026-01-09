c853500501444d5e516549c0fb846e8bef17b05b
Sharesth <sharesth444yt@gmail.com>
2026-01-09 23:34:24 +0530

    added a new function

--- cleaning/arbitrage.py
+++ cleaning/arbitrage.py
@@ -10,3 +10,17 @@ def calendar_arbitrage_check(df):
             violations.append(strike)
 
     return violations
+
+def butterfly_arbitrage(df):
+    violations = []
+
+    for T, grp in df.groupby("time_to_maturity"):
+        grp = grp.sort_values("strike")
+        strikes = grp["strike"].values
+        vols = grp["implied_vol"].values
+
+        second_derivative = np.diff(vols, 2)
+        if np.any(second_derivative < -0.01):
+            violations.append(T)
+
+    return violations
