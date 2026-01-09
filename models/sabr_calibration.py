ae4e6edbf290e841625072433e00313b3089a0f4
Sharesth <sharesth444yt@gmail.com>
2026-01-09 23:24:26 +0530

    added a new function

--- models/sabr_calibration.py
+++ models/sabr_calibration.py
@@ -1,3 +1,16 @@
 import numpy as np
 from scipy.optimize import minimize
-from .sabr import sabr_implied_vol
+from sabr import sabr_implied_vol
+
+def sabr_calibration(strikes, maturities, prices, F, r):
+    def objective(params):
+        alpha, beta, rho, nu = params
+        errors = []
+
+        for i in range(len(strikes)):
+            K = strikes[i]
+            T = maturities[i]
+            P = prices[i]
+            error = P - sabr_implied_vol(F, K, T, alpha, beta, rho, nu)
+            errors.append(error)
+        return np.sum(errors**2)
