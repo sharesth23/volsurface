af6c11f2454cc10120c6b3cfe9efd16688531d09
Sharesth <sharesth444yt@gmail.com>
2026-01-10 21:13:47 +0530

    added anew function for plot showing

---
+++ visualization/smile_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plot_smile(strikes , vols , maturity):
+    plt.figure(figsize=(10,6))
+    plt.plot(strikes, vols, label=f"Maturity: {maturity}")
+    plt.xlabel("Strike")
+    plt.ylabel("Implied Volatility")
+    plt.title(f"Implied Volatility Smile for Maturity: {maturity}")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
