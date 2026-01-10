143b57896c049ed61ce15ed5be0d490bc14e3dfe
Sharesth <sharesth444yt@gmail.com>
2026-01-10 21:35:38 +0530

    added a new function for svi file

---
+++ models/svi.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+def svi_total_variance(k , a, b, rho , m ,sigma):
+    return  a+b*(rho(k-m) = np.sqrt((k-m)**2 + sigma**2))
+
+def svi_implied_vol(F,K,T,params):
+    k = np.log(K/F)
+    w = svi_total_variance( k , **params)
+    return np.sqrt(w/T)
+
