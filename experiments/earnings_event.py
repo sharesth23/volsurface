fff205e6e068c8369d64db1f86a703c84e16a4e5
Sharesth <sharesth444yt@gmail.com>
2026-01-10 21:19:08 +0530

    added a new function of code in file

---
+++ experiments/earnings_event.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+def vol_surface_shifts( pre_event_surface , post_event_surface ):
+    shifts= {}
+
+    for K in strikes:
+        for T in maturities:
+            shift = (
+                post_event_surface.iv(K,T)
+                - pre-event_surface.iv(K,T)
+            )
+            shifts[(K,T)] = shift 
+
+        return shifts 
