54e9363c2c182633243930963ed0bc4a29aa9a9d
Sharesth <sharesth444yt@gmail.com>
2026-01-19 19:33:31 +0530

    written imports for api calls

--- volsurface/api.py
+++ volsurface/api.py
@@ -6,26 +6,6 @@ from volsurface.surface.surface_builder import VolSurface
 from volsurface.validation.no_arbitrage import validate_surface
 
 
-class VolSurface:
-    def __init__(self, option_chain, model="svi"):
-        self.option_chain = option_chain
-        self.model = model
-        self.surface = None
 
-    def calibrate(self):
-        if self.model == "svi":
-            params = calibrate_svi_surface(self.option_chain)
-        elif self.model == "sabr":
-            params = calibrate_sabr_surface(self.option_chain)
-        else:
-            raise ValueError("Model must be 'svi' or 'sabr'")
-
-        self.surface = VolSurfaceGrid(params, self.model)
-
-    def iv(self, strike, maturity):
-        return self.surface.iv(strike, maturity)
-
-    def diagnostics(self):
-        return surface_diagnostics(self.surface)
 
 
