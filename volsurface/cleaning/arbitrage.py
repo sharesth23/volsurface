b5083d464a9a21dabb8be6525583a7f1d48f76f8
Sharesth <sharesth444yt@gmail.com>
2026-01-19 19:42:20 +0530

    added a new function named as calendar_arbitrage_check

--- volsurface/cleaning/arbitrage.py
+++ volsurface/cleaning/arbitrage.py
@@ -1,26 +1,11 @@
 import pandas as pd 
+import numpy as np 
 
-def calendar_arbitrage_check(df):
-    df = df.sort_values(["strike", "time_to_maturity"])
+def calendar_arbitrage_check(df: pd.Dataframe):
+    df = df.copy()
+    df = df.sort_values(["strikes", "times_to_maturity"])
     violations = []
 
-    for strike, grp in df.groupby("strike"):
-        iv_diff = np.diff(grp["implied_vol"].values)
-        if np.any(iv_diff < -0.01):
-            violations.append(strike)
-
-    return violations
-
-def butterfly_arbitrage(df):
-    violations = []
-
-    for T, grp in df.groupby("time_to_maturity"):
-        grp = grp.sort_values("strike")
-        strikes = grp["strike"].values
-        vols = grp["implied_vol"].values
-
-        second_derivative = np.diff(vols, 2)
-        if np.any(second_derivative < -0.01):
-            violations.append(T)
-
-    return violations
+    for strike , grp in df.groupby ("strike")
+      T = grp["time_to_maturity"].values 
+      iv = grp["implied_vol"].values 
